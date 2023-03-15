7c042dd9125afad85238c704851f83ecad0ea78a
youssef tadlaoui <usftadlaoui@gmail.com>
2023-03-15 12:45:59 +0100

    basedonne&migrate

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -41,6 +41,7 @@ class UserController extends Controller
                 'name' => $request->name,
                 'email' => $request->email,
                 'password' => Hash::make($request->password),
+                'role_id'=>3
             ]);
         // $user = User::create([
         //     'name' => $request->get('name'),
